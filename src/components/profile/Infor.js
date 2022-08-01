26420e0eea99e9e2dc37dab63231f5dbe5b0e0fd
Thu <thu261999@gmail.com>
2022-08-01 07:40:04 +0700

    Minor update

--- src/components/profile/Infor.js
+++ src/components/profile/Infor.js
@@ -1,71 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { getProfileUsers } from '../../redux/actions/profileAction'
-import { Avatar } from 'antd';
-import AvatarCustom from '../AvatarCustom'
-import moment from 'moment';
-import FollowBtn from '../FollowBtn'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getProfileUsers } from "../../redux/actions/profileAction";
+import { Avatar, Button } from "antd";
+import AvatarCustom from "../AvatarCustom";
+import moment from "moment";
+import FollowBtn from "../FollowBtn";
 
 const Infor = () => {
-	const { id } = useParams()
-	const auth = useSelector(state => state.auth)
-	const dispatch = useDispatch()
-	const profile = useSelector(state => state.profile)
-	const [check, setCheck] = useState(false)
+  const { id } = useParams();
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.profile);
+  const [check, setCheck] = useState(false);
 
-	useEffect(() => {
-		dispatch(getProfileUsers({ id, auth }))
-		if (id === auth.user._id) {
-			setCheck(true)
-		} else {
-			setCheck(false)
-		}
-	}, [id, auth,dispatch])
+  useEffect(() => {
+    dispatch(getProfileUsers({ id, auth }));
+    if (id === auth.user._id) {
+      setCheck(true);
+    } else {
+      setCheck(false);
+    }
+  }, [id, auth, dispatch]);
 
-	/*   console.log(profile)
+  /*   console.log(profile)
     
 	  console.log(Object.keys(profile.user).length) */
 
-	if (Object.keys(profile.user).length === 0) return null;
+  if (Object.keys(profile.user).length === 0) return null;
 
-	return (
-		<div className="info">
-			<div className="info_container" key={profile.user._id}>
-				{Object.keys(profile.user.avatar).length > 0 ? <AvatarCustom src={profile.user.avatar.url} size="supper-avatar" /> :  <Avatar size={150}>USER</Avatar>}
-				
+  return (
+    <div className="info">
+      <div className="info_container" key={profile.user._id}>
+        {Object.keys(profile.user.avatar).length > 0 ? (
+          <AvatarCustom src={profile.user.avatar.url} size="supper-avatar" />
+        ) : (
+          <Avatar size={150}>USER</Avatar>
+        )}
 
-				<div className="info_content">
-					<div className="info_content_title">
-						<h2 style={{fontSize:'20px', fontWeight:'bold'}}>{profile.user.fullname}<span style={{fontSize:'16px'}}> ({profile.user.username})</span></h2>
-						{
-							profile.user._id === auth.user._id
-								? <button className="btn btn-outline-info"
-									/* onClick={() => setOnEdit(true)} */>
-									Edit Profile
-								</button>
+        <div className="info_content">
+          <div className="info_content_title">
+            <h2 style={{ fontSize: "20px", fontWeight: "bold" }}>
+              {profile.user.fullname}
+              <span style={{ fontSize: "16px" }}>
+                {" "}
+                ({profile.user.username})
+              </span>
+            </h2>
+          </div>
 
-								:  <FollowBtn user={profile.user} />
-						}
-					</div>
+          <div className="follow_btn">
+            <span className="mr-4" /* onClick={() => setShowFollowers(true)} */>
+              {profile.user.followers.length} Followers
+            </span>
+            <span className="ml-4" /* onClick={() => setShowFollowing(true)} */>
+              {profile.user.following.length} Following
+            </span>
+          </div>
+          <h6 className="m-0">{profile.user.email}</h6>
+          <h6 className="m-0">
+            {moment(profile.user.dob).format("DD/MM/YYYY")}
+          </h6>
+          <p className="m-0">{profile.user.address}</p>
+          <a href={profile.user.website} target="_blank" rel="noreferrer">
+            {profile.user.website}
+          </a>
+        </div>
 
-					<div className="follow_btn">
-						<span className="mr-4" /* onClick={() => setShowFollowers(true)} */>
-							{profile.user.followers.length} Followers
-						</span>
-						<span className="ml-4" /* onClick={() => setShowFollowing(true)} */>
-							{profile.user.following.length} Following
-						</span>
-					</div>
-					<h6 className="m-0">{profile.user.email}</h6>
-					<h6 className="m-0">{moment(profile.user.dob).format('DD/MM/YYYY')}</h6>
-					<p className="m-0">{profile.user.address}</p>
-					<a href={profile.user.website} target="_blank" rel="noreferrer">
-                        {profile.user.website}
-                    </a>
-				</div>
+        {profile.user._id === auth.user._id ? (
+          <Button shape='round' type='primary'
+            /* onClick={() => setOnEdit(true)} */
+          >
+            Edit Profile
+          </Button>
+        ) : (
+          <FollowBtn user={profile.user} />
+        )}
 
-{/* 				{
+        {/* 				{
 					onEdit && <EditProfile setOnEdit={setOnEdit} />
 				}
 
@@ -83,9 +95,9 @@ const Infor = () => {
 						setShowFollowing={setShowFollowing}
 					/>
 				} */}
-			</div>
-		</div>
-	)
-}
+      </div>
+    </div>
+  );
+};
 
-export default Infor
+export default Infor;
